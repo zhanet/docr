d0481229cd8f312a5889992cd449c260e174f773
zhanet <zhanet@163.com>
2018-10-25 15:34:41 +0800

    feat: remove known_hosts item

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -d -p 22:22 --name docr zhanet/wedocr
-ssh root@localhost
+docker run -d -p 33322:22 --name docr zhanet/wedocr 2>/dev/null
+# sed -i '/localhost/d' ~/.ssh/known_hosts
+sed -i '' '/33322/d' ~/.ssh/known_hosts
+ssh root@localhost -p 33322
