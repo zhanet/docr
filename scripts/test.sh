41e1c57bd64c827f4fbb062bee047ecbdb36e396
zhanet <zhanet@163.com>
2018-10-25 15:37:55 +0800

    fix: disable error message

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,8 @@ docker exec -it docr tesseract \-v
 docker exec -it docr tesseract \--list-langs
 
 # Base OCR test
-docker exec -it docr /bin/bash -c "echo +++; mkdir ./ocr-files/; cd ./ocr-files/; pwd; rm phototest.*; echo +++; \
-wget -O phototest.tif https://github.com/tesseract-ocr/test/raw/master/testing/phototest.tif; \
+docker exec -it docr /bin/bash -c "echo +++; mkdir ~/ocr-files/ >/dev/null 2>&1; cd ~/ocr-files/; \
+wget -O phototest.tif -q https://github.com/tesseract-ocr/test/raw/master/testing/phototest.tif; \
 tesseract phototest.tif phototest; \
 head -100 phototest.txt; \
 echo Compare the text above ^^^ with the source image: \ 
