251819fefb2d1501a7003925012f25bb271632dc
zhanet <zhanet@163.com>
2018-10-26 01:27:38 +0800

    refactor: change volume name

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker volume create pgdata
-docker volume create pgadata
+docker volume create postgres-data
+docker volume create pgadmin-data
 
 docker run --name docr-postgres \
--v pgdata:/var/lib/postgresql/data \
+-v postgres-data:/var/lib/postgresql/data \
 -e POSTGRES_PASSWORD=postgres \
 -p 5432:5432 --restart=always \
 -d mdillon/postgis
 
 docker run --link docr-postgres \
--v pgadata:/var/lib/pgadmin \
+-v pgadmin-data:/var/lib/pgadmin \
 -e "PGADMIN_DEFAULT_EMAIL=postgres@pgadmin.org" \
 -e "PGADMIN_DEFAULT_PASSWORD=postgres" \
 -p 5050:80 --name docr-pgadmin \
