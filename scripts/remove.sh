25d6cccb3594ead4ae64a77db182ed255641e40f
zhanet <zhanet@163.com>
2018-10-26 01:36:21 +0800

    refactor: optimize remove known_hosts line

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -2,3 +2,5 @@
 docker stop docr >/dev/null 2>&1
 docker rm docr >/dev/null 2>&1
 docker image rm zhanet/wedocr:latest >/dev/null 2>&1
+# sed -i '/localhost/d' ~/.ssh/known_hosts
+sed -i '' '/33322/d' ~/.ssh/known_hosts
